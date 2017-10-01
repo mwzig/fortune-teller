2c576085bcf33e8ab964c118ccc8a33f0ad19ebd
MZiegler <mwziegler2014@gmail.com>
2017-10-01 13:05:01 -0400

    finished FortuneTeller2 to show getting some input as integers and some as strings

--- src/FortuneTeller2.java
+++ src/FortuneTeller2.java
@@ -1,61 +1,75 @@
 import java.util.Scanner;
-// save to play around with how the input is working with new lines
 
+//********************************************************************************************
+//* Saved this - it shows how to get some of the input as integers and 
+//* some as strings.  There is a weirdness to getting an integer
+//* followed by getting a string.  
+//* When inputting data, you have to type in the integer and then hit enter so
+//* that the program knows you have finished entering the input.
+//* The nextInt() method just gets the integer. (But there is that enter (newline/CR) still out
+//* there.   The program's nextLine() then picks up that newline and thinks that is the  
+//* input for the nextLine(), and since it is a newline/CR, it thinks the data is finished 
+//* (with basically nothing entered).  To fix this, you have to add an extra nextLine()
+//* after a nextInt that is followed by a nextLine.
+//********************************************************************************************
 
 public class FortuneTeller2 {
 
-	
-	
-	
+	private String fName;
+	private String lName;
+	private int age;
+	private int birthMonth;
+	private int nbrSiblings;
+	private String favColor;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
-		//getUserInfo();
+
+		FortuneTeller2 fortTell2 = new FortuneTeller2();
+		fortTell2.getUserInfo();
 	}
-	
-	public static void getUserInfo() {
-	
-		// data input by the user
-		String fName;
-		String lName;
-		int age;
-		int birthMonth;
-		int nbrSiblings;
-		String favColor;
-		
-		Scanner input;
-		
-		input = new Scanner(System.in);
-		
+
+	// get data from the user
+	public void getUserInfo() {
+
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("Greetings!  We are here to tell your fortune!");
-		System.out.println("First, we will need to ask you for some information. " 
-		+ "(If at any time you want to end the program, enter \"quit\")\n");
-		
+		System.out.println("First, we will need to ask you for some information. "
+				+ "(If at any time you want to end the program, enter \"quit\")\n");
+
 		System.out.println("What is your first name?");
 		fName = input.nextLine();
-		
+
 		System.out.println("What is your last name?");
 		lName = input.nextLine();
-		
-		System.out.println("What is your birth month? (Enter in MM (1-12) format)");
-		birthMonth = input.nextInt();
-		
+
 		System.out.println("What is your age?");
 		age = input.nextInt();
-		
-		System.out.println("How many siblings do you have?");
+
+		System.out.println("What is your birth month? (Enter in MM (1-12) format)");
+		birthMonth = input.nextInt();
+
+		// add a nextLine() here because we are following a nextInt() with a nextLine()
+		// and if we don't do this, we will just pick up the /n after the integer as the
+		// value for the nextLine().
+		input.nextLine();
+
+		System.out.println("What is your favorite ROYGBIV color? " + "(Enter \"help\" to list the ROYGBIV colors)");
+		favColor = input.nextLine();
+
+		System.out.println("Now, for the final question! How many siblings do you have?");
 		nbrSiblings = input.nextInt();
 		input.nextLine();
-		
-		System.out.println("Now, for the final question! What is your favorite ROYGBIV color? "
-		+ "(Enter \"help\" to list the ROYGBIV colors)");
-		favColor = input.nextLine();
-		
-	
+
+		System.out.println(" The user's name is " + fName + " " + lName + ".");
+		System.out.println("Their age is " + age + ", birth month is " + birthMonth + ", favorite color is " + favColor
+				+ ", and number of siblings is " + nbrSiblings + ".");
+
 	}
-	
+
+	// we'll use this soon!
 	public static void checkForQuit(String userInput) {
-		
+
 		// if the user enters "quit" for any question, stop running.
 		if (userInput.equals("quit")) {
 			System.exit(0);
